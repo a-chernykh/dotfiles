2ed7e319bcb79c6f73d71e9194b50832d4eb10d0
Andrey Chernih <andrey.chernih@gmail.com>
2012-08-04 23:39:14 +0400

    Ignore vim backups

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 include_line="[ -f ${script_dir}/bashrc ] && . ${script_dir}/bashrc"
-echo $include_line
 (grep "${include_line}" ~/.bashrc > /dev/null 2>&1) || (echo "${include_line}" >> ~/.bashrc)
 
 ln -s ${script_dir}/irbrc ~/.irbrc
