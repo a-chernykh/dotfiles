ec569255e0278e2ccaf3903cce7a9c28d4cbed7c
Andrey Chernih <andrey.chernih@gmail.com>
2012-11-24 22:26:57 +0400

    Refactor install script

--- install.sh
+++ install.sh
@@ -5,11 +5,8 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 include_line="[ -f ${script_dir}/bashrc ] && . ${script_dir}/bashrc"
 (grep "${include_line}" ~/.bashrc > /dev/null 2>&1) || (echo "${include_line}" >> ~/.bashrc)
 
-ln -s ${script_dir}/irbrc ~/.irbrc
-ln -s ${script_dir}/screenrc ~/.screenrc
-ln -s ${script_dir}/gemrc ~/.gemrc
-ln -s ${script_dir}/vimrc ~/.vimrc
-ln -s ${script_dir}/gitconfig ~/.gitconfig
-ln -s ${script_dir}/gitignore ~/.gitignore_global
-ln -s ${script_dir}/caprc ~/.caprc
-ln -s ${script_dir}/rspec ~/.rspec
+dotfiles=( irbrc screenrc gemrc vimrc gitconfig gitignore_global caprc rspec rvmrc )
+for dotfile in "${dotfiles[@]}"
+do
+  ln -sf ${script_dir}/${dotfile} ~/.${dotfile}
+done
