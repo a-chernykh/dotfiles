3dd40229695dda60131854891fc41a79018689cd
Andrey Chernih <andrey.chernih@gmail.com>
2013-06-09 22:50:19 +0400

    Add tmux config

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 include_line="[ -f ${script_dir}/bashrc ] && . ${script_dir}/bashrc"
 (grep "${include_line}" ~/.bashrc > /dev/null 2>&1) || (echo "${include_line}" >> ~/.bashrc)
 
-dotfiles=( irbrc screenrc gemrc vimrc gitconfig gitignore_global caprc rspec rvmrc )
+dotfiles=( irbrc screenrc gemrc vimrc gitconfig gitignore_global caprc rspec rvmrc .tmux.conf )
 for dotfile in "${dotfiles[@]}"
 do
   ln -sf ${script_dir}/${dotfile} ~/.${dotfile}
