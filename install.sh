cdc1640766f9e11e064078e706344cf8ab8e7c20
Andrey Chernih <andrey.chernih@gmail.com>
2015-03-10 13:13:50 -0700

    Add --drb flag to RSpec builder

--- install.sh
+++ install.sh
@@ -17,4 +17,5 @@ SUBLIME_CONFIG_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/Us
 if [ -d "$SUBLIME_CONFIG_DIR" ]; then
   ln -sf "$SCRIPT_DIR/sublime/Package Control.sublime-settings" "$SUBLIME_CONFIG_DIR/Package Control.sublime-settings"
   ln -sf "$SCRIPT_DIR/sublime/Preferences.sublime-settings" "$SUBLIME_CONFIG_DIR/Preferences.sublime-settings"
+  ln -sf "$SCRIPT_DIR/sublime/RSpec.sublime-build" "$SUBLIME_CONFIG_DIR/RSpec.sublime-build"
 fi
