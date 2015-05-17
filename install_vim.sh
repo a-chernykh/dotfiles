fff9ee27a45cc1456d99e7ebe116e84d482f844c
Andrey Chernih <andrey.chernih@gmail.com>
2015-05-17 14:41:29 -0700

    Add ctrlp-cmatcher and nerdtree

--- install_vim.sh
+++ install_vim.sh
@@ -7,3 +7,5 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 [ -d ~/.vim/bundle/vim-sensible ] || git clone git://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
 [ -d ~/.vim/bundle/ctrlp.vim ] || git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/ctrlp.vim
+[ -d ~/.vim/bundle/ctrlp-cmatcher ] || git clone https://github.com/JazzCore/ctrlp-cmatcher ~/.bim/bundle/ctrlp-cmatcher
+[ -d ~/.vim/bundle/nerdtree ] || git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
