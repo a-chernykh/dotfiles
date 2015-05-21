db1403ae33f8c2abe1a308eb36020777cf09ab07
Andrey Chernih <andrey.chernih@gmail.com>
2015-05-21 13:31:13 -0700

    Add ackrc

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INCLUDE_LINE="[ -f ${SCRIPT_DIR}/bashrc ] && . ${SCRIPT_DIR}/bashrc"
 (grep "${INCLUDE_LINE}" ~/.bashrc > /dev/null 2>&1) || (echo "${INCLUDE_LINE}" >> ~/.bashrc)
 
-dotfiles=( irbrc screenrc gemrc vimrc gitconfig gitignore_global caprc rspec tmux.conf )
+dotfiles=( irbrc screenrc gemrc vimrc gitconfig gitignore_global caprc rspec tmux.conf ackrc )
 for dotfile in "${dotfiles[@]}"
 do
   ln -sf ${SCRIPT_DIR}/${dotfile} ~/.${dotfile}
 done
 
-install_sublime.sh
-install_vim.sh
+./install_sublime.sh
+./install_vim.sh
