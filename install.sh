6561bfb865a73462249f450316b3aa45df9f2c93
Andrey Chernih <andrey.chernih@gmail.com>
2017-08-23 10:54:27 -0700

    Add agignore

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-dotfiles=( irbrc screenrc gemrc vimrc gitconfig gitignore_global caprc rspec tmux.conf ackrc ctags )
+dotfiles=( irbrc screenrc gemrc vimrc gitconfig gitignore_global caprc rspec tmux.conf ackrc ctags agignore )
 for dotfile in "${dotfiles[@]}"
 do
   ln -sf ${SCRIPT_DIR}/${dotfile} ~/.${dotfile}
